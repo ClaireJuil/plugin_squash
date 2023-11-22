a2f83d3a8d452c9f34b33fa1746c3e0a0b2cab06
Claire JUILLARD <cjuillard@NX-DOMAIN.LAN>
2023-11-22 10:06:15 +0100

    creation projet

---
+++ src/main/java/org/squashtest/tm/plugin/custom/export/convergence/Parser.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright ANS 2020-2022
+ */
+package org.squashtest.tm.plugin.custom.export.convergence;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.nodes.Entities.EscapeMode;
+import org.jsoup.safety.Safelist;
+//import org.jsoup.safety.Whitelist;
+import org.jsoup.select.Elements;
+
+/**
+ * The Class Parser.
+ */
+public class Parser {
+
+	private Parser() {
+	};
+	/**
+	 * Convert HTML to string.
+	 *
+	 * @param html the html
+	 * @return the string
+	 */
+	public static String convertHTMLtoString(String html) {
+/*		
+		String tmp = "";
+		if (html == null || html.isEmpty()) {
+			return tmp;
+		}
+		//On commence par supprimer tous les retours chariot en trop.
+		String sanitizedHtml = html.replaceAll("\\r", "")
+		.replaceAll("\\n", "")
+		.replaceAll("\\t", "");
+		Document doc = Jsoup.parse(sanitizedHtml);
+		Document.OutputSettings outputSettings = new Document.OutputSettings();
+		outputSettings.escapeMode(EscapeMode.xhtml);
+		outputSettings.prettyPrint(false);
+		doc.outputSettings(outputSettings);
+		// traitement des listes ordonnées
+		Elements ol = doc.select("ol");
+		for (Element orderedLists : ol) {
+			Elements items = orderedLists.children();
+			int number;
+			// Traitement des listes qui ne commencent pas à 1
+			if(orderedLists.hasAttr("start")) {
+				number = Integer.parseInt(orderedLists.attr("start"));
+			}else {
+				number = 1;
+			}
+			for (Element item : items) {
+				item.before("\\n" + number + ".");
+				number++;
+			}
+		}
+		// traitement des listes simples
+		Elements ul = doc.select("ul");
+		for (Element orderedLists : ul) {
+			Elements items = orderedLists.children();
+			for (Element item : items) {
+				item.before("\\n" + Constantes.PREFIX_ELEMENT_LISTE_A_PUCES);
+			}
+		}
+		// traitement des paragraphes et retours à la ligne
+		doc.select("br").before("\\n");
+		doc.select("p").before("\\n");
+		doc.select("p").after("\\n");
+		doc.select("ol").after("\\n\\n");
+		doc.select("ul").after("\\n\\n");
+		String str = doc.html().replaceAll("\\\\n", "\n");		
+		
+		//a revoir en version TM 5.1.0 
+		//return Jsoup.clean(str, "", Safelist.relaxed(), outputSettings) 
+		//version TM 3.0.5
+		return Jsoup.clean(str,"", WhiteList.none(), outputSettings)
+				.replaceAll("&apos;", "'")
+				.replaceAll("&quot;", "\"")
+				.replaceAll("&gt;", ">")
+				.replaceAll("&lt;", "<")
+				.replaceAll("&amp;", "&");
+*/				
+		return html;
+	}
+}
---
+++ src/main/java/org/squashtest/tm/plugin/custom/export/convergence/SegurExcelReport.java
@@ -0,0 +1,27 @@
+/*
+ * Copyright ANS 2020-2022
+ */
+package org.squashtest.tm.plugin.custom.export.convergence;
+
+import java.io.File;
+import java.util.Map;
+
+import javax.inject.Inject;
+
+import org.squashtest.tm.api.report.BasicDirectDownloadableReport;
+import org.squashtest.tm.api.report.criteria.Criteria;
+import org.squashtest.tm.plugin.custom.export.convergence.service.ReportGeneratorService;
+
+/**
+ * The Class SegurExcelReport.
+ */
+public class SegurExcelReport extends BasicDirectDownloadableReport {
+
+	@Inject
+	ReportGeneratorService reportGeneratorService;
+
+	@Override
+	public File generateReport(Map<String, Criteria> criterias) {
+		return reportGeneratorService.generateReport(criterias);
+	}
+}
